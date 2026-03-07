281e37b05e6317c1ac6f50b9bd971dfea51eb871
dev-karishma <karishmamalve01@gmail.com>
2026-03-07 19:13:40 +0530

    first commit

--- src/components/Partners/Partners.jsx
+++ src/components/Partners/Partners.jsx
@@ -93,7 +93,7 @@ const Partners = () => {
 
                         return (
                             <div className="circle" key={i}>
-                                <img src={logo} alt="partner" />
+                                <img src={Podar} alt="partner" />
                             </div>
                         );
 
